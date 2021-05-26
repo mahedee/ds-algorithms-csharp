140e20514079ac629764d3265b767285a0682515
mahedee <mahedee.hasan@gmail.com>
2021-05-26 14:30:42 +0600

    bst in progress

---
+++ csharp/BST/BST/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BST
+{
+    public class Node
+    {
+        public int data;
+        Node parent;
+        Node left;
+        Node right;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
